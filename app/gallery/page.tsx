0517b8d31f9c70081cd856ece82564498b4056c9
kyle <kyle@kylelee.dev>
2023-05-08 15:26:34 -0500

    added more 3D assets

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -28,7 +28,34 @@ const sceneObjects: SceneObjectType[] = [
     type: 'planet',
     children: [],
   },
-  { name: 'sun', position: new THREE.Vector3(0, 0, -55), scale: 0.025, type: 'planet', children: [] },
+  { name: 'sun', position: new THREE.Vector3(10, 0, -35), scale: 0.025, type: 'planet', children: [] },
+  { name: 'juno', position: new THREE.Vector3(0, 0, -55), scale: 0.025, type: 'planet', children: [] },
+  { name: 'saturn', position: new THREE.Vector3(20, 0, 105), scale: 0.025, type: 'planet', children: [] },
+  { name: 'rhea', position: new THREE.Vector3(-100, 0, -300), scale: 0.025, type: 'planet', children: [] },
+  { name: 'uranus', position: new THREE.Vector3(-100, 0, 100), scale: 0.025, type: 'planet', children: [] },
+  {
+    name: 'earth',
+    position: new THREE.Vector3(-50, 0, -75),
+    scale: 0.025,
+    type: 'planet',
+    children: [{ name: 'iss', position: new THREE.Vector3(-2000, 0, -75), scale: 3, type: 'satellite', children: [] }],
+  },
+
+  {
+    name: 'swordfish',
+    position: new THREE.Vector3(-50, 0, 70),
+    scale: 1,
+    type: 'ship',
+    children: [{ name: 'redtail', position: new THREE.Vector3(-10, 0, 10), scale: 1, type: 'ship', children: [] }],
+  },
+
+  {
+    name: 'mandalorian',
+    position: new THREE.Vector3(30, 0, 70),
+    scale: 1,
+    type: 'ship',
+    children: [{ name: 'tiefighter', position: new THREE.Vector3(30, 0, 10), scale: 1, type: 'ship', children: [] }],
+  },
 ]
 const Galaxy = dynamic(() => import('@/components/canvas/Galaxy').then((mod) => mod.Galaxy), { ssr: false })
 const Rocket = dynamic(() => import('@/components/canvas/Rocket').then((mod) => mod.Rocket), { ssr: false })
