e53cade46f812bc49877edb50548df6d6c84115c
kyle <kyle@kylelee.dev>
2023-05-13 21:16:18 -0500

    working physics based movement

--- src/components/canvas/Rocket.tsx
+++ src/components/canvas/Rocket.tsx
@@ -3,9 +3,12 @@ import { useFrame, useLoader, useThree } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useKeyboardControls } from '@react-three/drei'
 import * as THREE from 'three'
-import { CuboidCollider, RigidBody, RapierCollider, useRapier, vec3 } from '@react-three/rapier'
+import { CuboidCollider, RigidBody, RapierCollider, useRapier, quat, vec3, euler } from '@react-three/rapier'
 import { KinematicCharacterController } from '@dimforge/rapier3d-compat'
+import { random16 } from 'three/src/math/MathUtils'
 //Some helpers to get the camera working correctly
+
+const MOVEMENT_SPEED = 10
 const calculateIdealLookAt = (rotation: THREE.Euler, position: THREE.Vector3) => {
   const idealLookAt = new THREE.Vector3(2, 2, 2)
   idealLookAt.applyEuler(rotation)
@@ -31,94 +34,70 @@ export function Rocket() {
     velocity: vec3(),
   })
   const controller = useRef<KinematicCharacterController>()
-  const { clock } = three
-  const elapsedTime = clock.elapsedTime
-  const currPosition = new THREE.Vector3()
-  const currLookAt = new THREE.Vector3()
   const rapier = useRapier()
 
   const collider = useRef<RapierCollider>(null)
-  console.log(rapier.world.raw().createCharacterController(0.1))
   const [subscribeKeys, getKeys] = useKeyboardControls()
-  //init controller
-  useEffect(() => {
-    const c = rapier.world.raw().createCharacterController(0.1)
-    c.setApplyImpulsesToDynamicBodies(true)
-    c.setCharacterMass(0.2)
-    c.enableSnapToGround(0.02)
-    controller.current = c
-  }, [rapier])
-
-  useFrame((context, delta) => {
-    if (controller.current && rigidBody.current && collider.current) {
-      try {
-        const { velocity } = refState.current
-
-        const position = vec3(rigidBody.current.translation())
-        const movement = vec3()
-
-        // if (keysdown.current.ArrowUp) {
-        //   movement.z -= 0.07;
-        // }
-        // if (keysdown.current.ArrowDown) {
-        //   movement.z += 0.07;
-        // }
-        // if (keysdown.current.ArrowLeft) {
-        //   movement.x -= 0.07;
-        // }
-        // if (keysdown.current.ArrowRight) {
-        //   movement.x += 0.07;
-        // }
-        //
-        // if (refState.current.grounded && keysdown.current[" "]) {
-        //   velocity.y = 0.2;
-        // }
-        //
-        // if (!refState.current.grounded) {
-        //   // Apply gravity
-        //   velocity.y -= (9.807 * delta) / 20;
-        // }
-
-        movement.add(velocity)
-
-        controller.current.computeColliderMovement(collider.current, movement)
-        refState.current.grounded = controller.current.computedGrounded()
-
-        let correctedMovement = controller.current.computedMovement()
-        position.add(vec3(correctedMovement))
-
-        rigidBody.current.setNextKinematicTranslation(position)
-      } catch (err) {}
-    }
-  })
-
+  // //init controller
   // useEffect(() => {
-  //   //rotate the rocket so that it's sideways
-  //   rocketGroup.current.rotation.set(-1.5, 0, 0)
-  // }, [rocketGroup, rocket])
-  //
-  // useFrame((state, delta) => {
-  //   const { forward, left, right } = getKeys()
-  //   if (forward) {
-  //     rocketGroup.current.translateY(0.5)
-  //   }
-  //   if (left) {
-  //     const angleOfRotation = Math.PI / 100
-  //     rocketGroup.current.rotateZ(angleOfRotation)
-  //   }
-  //   if (right) {
-  //     const angleOfRotation = -Math.PI / 100
-  //     rocketGroup.current.rotateZ(angleOfRotation)
-  //   }
-  // })
-  //
-  //chase camera
+  //   const c = rapier.world.raw().createCharacterController(0.1)
+  //   c.setApplyImpulsesToDynamicBodies(true)
+  //   c.setCharacterMass(0.2)
+  //   c.enableSnapToGround(0.02)
+  //   controller.current = c
+  //   rocket.current.rotateX(-Math.PI / 2)
+  // }, [rapier])
+
+  useFrame((state, delta) => {
+    const { forward, left, right } = getKeys()
+    try {
+      // const position = vec3(rigidBody.current.translation())
+      // const rotation = quat(rigidBody.current.rotation())
+      // const eulerRot = euler().setFromQuaternion(quat(rigidBody.current.rotation()))
+      // const idealLookAt = calculateIdealLookAt(eulerRot, position)
+      // const idealOffset = calculateIdealOffset(eulerRot, position)
+      // const movement = vec3()
+
+      const impulse = { x: 0, y: 0, z: 0 }
+      const torque = { x: 0, y: 0, z: 0 }
+      let rotate = false
+      const impulseStrength = 3
+      const torqueStrength = 0.2 * delta
+      if (forward) {
+        impulse.z -= impulseStrength
+        rotate = true
+        console.log('here')
+      }
+      if (left) {
+        impulse.x -= impulseStrength
+        rotate = true
+      }
+      if (right) {
+        impulse.x += impulseStrength
+        rotate = true
+      }
+
+      rigidBody.current.applyImpulse(impulse)
+      // movement.add(velocity)
+      // controller.current.computeColliderMovement(collider.current, movement)
+      // refState.current.grounded = controller.current.computedGrounded()
+      // let correctedMovement = controller.current.computedMovement()
+      // position.add(vec3(correctedMovement))
+      // rigidBody.current.setNextKinematicTranslation(position)
+      // rigidBody.current.setNextKinematicRotation(rotation)
+      //      console.log(rigidBody.current.linvel())
+      if (rotate) {
+        const linvel = rigidBody.current.linvel()
+        const angle = Math.atan2(linvel.x, linvel.z)
+        rigidBody.current.rotation.y = angle
+      }
+    } catch (err) {}
+  })
 
   const gltf = useLoader(GLTFLoader, '/rocket.glb')
 
   return (
-    <RigidBody type='kinematicPosition' ref={rigidBody} colliders={false}>
-      <CuboidCollider args={[2, 2, 2]} ref={collider} />
+    <RigidBody ref={rigidBody} colliders='cuboid'>
       <group ref={rocketGroup}>
         <primitive ref={rocket} position={[0, 0, 0]} object={gltf.scene} scale={1}></primitive>
       </group>
